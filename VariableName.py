66148681361b83af57c2ef9bdba585518247e38c
thisisdishang <dishangkumarrana@gmail.com>
2023-07-28 12:06:09 +0530

    Day 2

---
+++ VariableName.py
@@ -0,0 +1,32 @@
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+
+myvar = "Dishang"
+my_var = "Dishang"
+_my_var = "Dishang"
+myVar = "Dishang"
+MYVAR = "Rana"
+myvar2 = "Dishang"
+
+print(_my_var)
+print(MYVAR)
+
+# Multi Words Variable Names
+
+# Camel Case
+# Each word, except the first, starts with a capital letter
+myVarName='Rean'
+
+# Pascal Case
+# Each word starts with a capital letter
+MyVarName='Hela'
+
+# Snake Case
+# Each word is separated by an underscore character
+my_var_name='Unak'
+
+
+
--- Variables.py
+++ Variables.py
@@ -1,4 +1,5 @@
 # Python has no command for declaring a variable
+# Variables are containers for storing data values.
 # Variables do not need to be declared with any particular type, and can even change type after they have been set.
 
 x=2
@@ -8,7 +9,6 @@ print(y)
 
 # Casting
 # If you want to specify the data type of a variable, this can be done with casting.
-
 x = str(3)    # x will be '3'
 print(x)
 y = int(3)    # y will be 3
@@ -17,7 +17,6 @@ z = float(3)  # z will be 3.0
 print(z)
 
 # You can get the data type of a variable with the type() function.
-
 print(type(x))
 print(type(y))
 print(type(z))
@@ -29,3 +28,41 @@ b="Rana"
 # Variable names are case-sensitive
 a="a"
 A=45
+
+# Assign Multiple Values
+# Many Values To Multiple Variables
+x,y,z="Dishang","Rana","07"
+print(x)
+print(y)
+print(z)
+
+# One Value To Multiple Variables
+x=y=z="Khaer"
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+# If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+colors=["Red","Blue","Green"]
+a,b,c=colors
+print(a)
+print(b)
+print(c)
+
+# You output multiple variables, separated by a comma
+p="I"
+q="Am"
+r="Indestructible"
+s=3
+print(p,q,r,s)
+
+# You can also use the + operator to output multiple variables
+# Space are excluded in + operator
+print(p+q+r)
+
+# Note:when you try to combine a string and a number with the + operator, Python will give you an error
+# the + character works as a mathematical operator
+a=10
+b=2
+print(a+b)
