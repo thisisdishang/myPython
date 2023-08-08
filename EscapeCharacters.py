ae2f7a6d49a93fa7991c88013b59d765587eff95
thisisdishang <dishangkumarrana@gmail.com>
2023-08-08 17:56:24 +0530

    Day 7

---
+++ EscapeCharacters.py
@@ -0,0 +1,21 @@
+# To insert characters that are illegal in a string, use an escape character
+# An escape character is a backslash \ followed by the character you want to 
+
+# The escape character allows you to use double quotes when you normally would not be allowed
+txt="She is my \'best friend\'"
+print(txt)
+
+path="C:\\me\\you\\we\\"
+print(path)
+
+# Other escape characters used in Python
+# Code	    Result
+# \'	    Single Quote	
+# \\	    Backslash	
+# \n	    New Line	
+# \r	    Carriage Return	
+# \t	    Tab	
+# \b	    Backspace	
+# \f	    Form Feed	
+# \ooo	    Octal value	
+# \xhh	    Hex value
