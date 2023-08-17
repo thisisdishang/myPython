564696a6c243fc0bc722432e70ecbedc3f6dcecd
thisisdishang <dishangkumarrana@gmail.com>
2023-08-17 18:37:42 +0530

    Day 11

---
+++ Operators.py
@@ -0,0 +1,51 @@
+# Operators are used to perform operations on variables and values
+
+# Python divides the operators in the following groups:
+# Arithmetic operators
+# Assignment operators
+# Comparison operators
+# Logical operators
+# Identity operators
+# Membership operators
+# Bitwise operators
+
+# Arithmetic operators are used with numeric values to perform common mathematical operations
+# Operator	Name	        Example
+#    +	    Addition	    x + y	
+#    -	    Subtraction	    x - y	
+#    *	    Multiplication	x * y	
+#    /	    Division	    x / y	
+#    %	    Modulus	        x % y	
+#    **	    Exponentiation	x ** y	
+#    //	    Floor division	x // y
+
+# Assignment operators are used to assign values to variables
+# Operator	Example	    Same As
+#    =	    x = 5	    x = 5	
+#   +=	    x += 3	    x = x + 3	
+#   -=	    x -= 3	    x = x - 3	
+#   *=	    x *= 3	    x = x * 3	
+#   /=	    x /= 3	    x = x / 3	
+#   %=	    x %= 3	    x = x % 3	
+#   //=	    x //= 3	    x = x // 3	
+#   **=	    x **= 3	    x = x ** 3	
+#   &=	    x &= 3	    x = x & 3	
+#   |=	    x |= 3	    x = x | 3	
+#   ^=	    x ^= 3	    x = x ^ 3	
+#   >>=	    x >>= 3	    x = x >> 3	
+#   <<=	    x <<= 3	    x = x << 3
+
+# Comparison operators are used to compare two values
+# Operator	Name	                    Example
+#   ==	    Equal	                    x == y	
+#   !=	    Not equal	                x != y	
+#   >	    Greater than	            x > y	
+#   <	    Less than	                x < y	
+#   >=	    Greater than or equal to	x >= y	
+#   <=	    Less than or equal to	    x <= y
+
+# Logical operators are used to combine conditional statement
+# Operator	Description	                                                    Example
+#   and 	Returns True if both statements are true	                    x < 5 and x < 10	
+#   or	    Returns True if one of the statements is true	                x < 5 or x < 4	
+#   not	    Reverse the result, returns False if the result is true	not     (x < 5 and x < 10)
